b914f24e60a5347850e9ecac36194cf422a7fd27
Suyog Gour <52906463+Suyog64@users.noreply.github.com>
2025-01-27 22:31:30 +0530

    Allocate min number of pages to students

---
+++ AllocateMinNoOfPages.java
@@ -0,0 +1,62 @@
+package com.test;
+
+public class AllocateMinNoOfPages {
+
+	public static int minNoOfPages(int arr[], int k) {
+		int n = arr.length;
+		int start = maxOfArray(arr);
+		int end = sumOfArray(arr);
+		int res = -1;
+		while (start <= end) {
+			int mid = start + (end - start) / 2;
+			if (isValid(arr, n, k, mid) == true) {
+				res = mid;
+				end = mid - 1;
+			} else {
+				start = mid + 1;
+			}
+		}
+		return res;
+	}
+
+	private static boolean isValid(int[] arr, int n, int k, int max) {
+		int student = 1;
+		int sum = 0;
+		for (int i = 0; i < n; i++) {
+			sum = sum + arr[i];
+			if (sum > max) {
+				student++;
+				sum = arr[i];
+			}
+			if (student > k) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	private static int sumOfArray(int[] arr) {
+		int sum = 0;
+		for (int i = 0; i < arr.length; i++) {
+			sum = sum + arr[i];
+		}
+		return sum;
+	}
+
+	private static int maxOfArray(int[] arr) {
+		int max = -1;
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] > max) {
+				max = arr[i];
+			}
+		}
+		return max;
+	}
+
+	public static void main(String[] args) {
+		int a[] = { 10, 20, 30, 40 };
+		int k = 2;
+		System.out.println("Minimum no of pages student to allocate:" + minNoOfPages(a, k));
+	}
+
+}
